4690f0124f8fa950b414b4e09f237997247cf689
karina <karina.zhoray@gmail.com>
2021-02-17 09:28:06 +0600

    feat: add function reverse (n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let a = n > 0 ? n : -n;
+    let i = a.toString().split('').reverse().join('');
+    return +i;
 }
